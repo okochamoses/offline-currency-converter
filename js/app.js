10a58bf9254d182efe3b6dcd427d9256d612761e
Moses Okocha <okocha.chukwutem@gmail.com>
2018-06-27 11:21:06 +0100

    Fetch currencies from api and sort into alphabetical order

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 //////////////////////////////
 //// APP LOGIC GOES HERE ////
 ////////////////////////////
+
+// Fetch all currencies
+fetch("https://free.currencyconverterapi.com/api/v5/currencies")
+  .then(response => response.json())
+  .then(currencies => {
+    // Sort object keys alphabetically into an array
+    const sortedCurrencyKeys = Object.keys(currencies.results).sort();
+
+    const sortedCurrencies = sortedCurrencyKeys.map(key => {
+      const currencyObject = new Object();
+      currencyObject[key] = currencies.results[key].currencyName;
+
+      return currencyObject;
+    });
+
+    console.log(sortedCurrencies);
+  });
