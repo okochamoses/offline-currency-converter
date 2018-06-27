26316ffc16abe2e967429faf67716a8a92d33347
Moses Okocha <okocha.chukwutem@gmail.com>
2018-06-27 12:02:57 +0100

    Add append currencies to DOM elements

--- js/app.js
+++ js/app.js
@@ -3,18 +3,41 @@
 ////////////////////////////
 
 // Fetch all currencies
-fetch("https://free.currencyconverterapi.com/api/v5/currencies")
-  .then(response => response.json())
-  .then(currencies => {
-    // Sort object keys alphabetically into an array
-    const sortedCurrencyKeys = Object.keys(currencies.results).sort();
+const fetchCurrencies = () => {
+  fetch("https://free.currencyconverterapi.com/api/v5/currencies")
+    .then(response => response.json())
+    .then(currencies => {
+      // Sort object keys alphabetically into an array
+      const sortedCurrencyKeys = Object.keys(currencies.results).sort();
 
-    const sortedCurrencies = sortedCurrencyKeys.map(key => {
-      const currencyObject = new Object();
-      currencyObject[key] = currencies.results[key].currencyName;
+      const sortedCurrencies = sortedCurrencyKeys.map(key => {
+        const currencyObject = new Object();
+        currencyObject[key] = currencies.results[key].currencyName;
 
-      return currencyObject;
+        return currencyObject;
+      });
+
+      // Get elements from DOM
+      const fromCurrency = document.getElementById("fromCurrency");
+      const toCurrency = document.getElementById("toCurrency");
+
+      // Append currencies to Select elements on DOM
+      sortedCurrencies.forEach(currency => {
+        const fromCurrencyOption = document.createElement("option");
+        const toCurrencyOption = document.createElement("option");
+        for (const item in currency) {
+          fromCurrencyOption.text = toCurrencyOption.text = `${item} (${
+            currency[item]
+          })`;
+          fromCurrencyOption.value = toCurrencyOption.value = item;
+        }
+        fromCurrency.appendChild(fromCurrencyOption);
+        toCurrency.appendChild(toCurrencyOption);
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
+};
 
-    console.log(sortedCurrencies);
-  });
+fetchCurrencies();
